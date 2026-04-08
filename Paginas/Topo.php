776bf7262688d674d9642d06493ee40973e1e9fd
Vinicius <viny25brasil@gmail.com>
2026-04-08 20:27:01 -0300

    feito

--- Paginas/Topo.php
+++ Paginas/Topo.php
@@ -9,7 +9,7 @@
             <nav class="menu-topo">
                 <a href="#" class="ativo">Início</a>
                 <a href="#servicos">Serviços</a>
-                <a href="agendar.php">Agendar</a>
+                <a href="Paginas/Agendar.php">Agendar</a>
                 <a href="area_admin.php" class="btn-admin">Área Admin</a>
             </nav>
         </div>
