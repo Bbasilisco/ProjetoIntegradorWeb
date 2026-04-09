18a5d12ccc9521ab8aa4b6c9d94e666467552987
Beatriz Basilisco <beatrizfonsecabasilisco@gmail.com>
2026-04-09 12:46:32 -0300

    Fix stylesheet link and update header structure

--- Paginas/Topo.php
+++ Paginas/Topo.php
@@ -1,16 +1,18 @@
-<link rel="stylesheet" href="../FrontEnd/Estilo.css">
-<header class="index-topo">
-        <div class="index-container topo-conteudo">
-            <div class="logo-marca">
-                <div class="logo-box">🔧</div>
-                <span>JP Auto Center</span>
-            </div>
+<link rel="stylesheet" href="FrontEnd/Estilo.css">
 
-            <nav class="menu-topo">
-                <a href="Index.php" class="ativo">Início</a>
-                <a href="#servicos">Serviços</a>
-                <a href="Paginas/Agendar.php">Agendar</a>
-                <a href="Paginas/AreaAdmin.php" class="btn-admin">Área Admin</a>
-            </nav>
+<header class="index-topo">
+    <div class="index-container topo-conteudo">
+        <div class="logo-marca">
+            <div class="logo-box">🔧</div>
+            <span>JP Auto Center</span>
         </div>
-    </header>
+
+        <nav class="menu-topo">
+            <a href="../Index.php" class="ativo">Início</a>
+            <a href="Index.php#servicos">Serviços</a>
+            
+            <a href="Paginas/Agendar.php">Agendar</a>
+            <a href="Paginas/AreaAdmin.php" class="btn-admin">Área Admin</a>
+        </nav>
+    </div>
+</header>
