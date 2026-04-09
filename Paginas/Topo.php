eb7fb9ccc7750100d2facfae17009d052be26cd0
Beatriz Basilisco <beatrizfonsecabasilisco@gmail.com>
2026-04-09 13:32:58 -0300

    Update stylesheet links and navigation paths

--- Paginas/Topo.php
+++ Paginas/Topo.php
@@ -1,4 +1,13 @@
-<link rel="stylesheet" href="FrontEnd/Estilo.css">
+<?php
+// Identifica se o arquivo que executou este código está na raiz ou na pasta Paginas
+$diretorioAtual = basename(dirname($_SERVER['PHP_SELF']));
+
+// Se estiver em 'Paginas', o prefixo para voltar à raiz é '../'
+// Caso contrário (na raiz), o prefixo é vazio ''
+$prefixo = ($diretorioAtual == 'Paginas') ? '../' : '';
+?>
+
+<link rel="stylesheet" href="<?php echo $prefixo; ?>FontEnd/Estilo.css">
 
 <header class="index-topo">
     <div class="index-container topo-conteudo">
@@ -8,11 +17,13 @@
         </div>
 
         <nav class="menu-topo">
-            <a href="../Index.php" class="ativo">Início</a>
-            <a href="Index.php#servicos">Serviços</a>
+            <a href="<?php echo $prefixo; ?>Index.php" class="ativo">Início</a>
             
-            <a href="Paginas/Agendar.php">Agendar</a>
-            <a href="Paginas/AreaAdmin.php" class="btn-admin">Área Admin</a>
+            <a href="<?php echo $prefixo; ?>Index.php#servicos">Serviços</a>
+            
+            <a href="<?php echo ($diretorioAtual == 'Paginas') ? '' : 'Paginas/'; ?>Agendar.php">Agendar</a>
+            
+            <a href="<?php echo ($diretorioAtual == 'Paginas') ? '' : 'Paginas/'; ?>AreaAdmin.php" class="btn-admin">Área Admin</a>
         </nav>
     </div>
 </header>
