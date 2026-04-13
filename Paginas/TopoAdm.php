3a90afff498d0bff946c5ba2111057cffc7e5f79
Gabrielmktt <gabrielbispodeabrantes1212@gmail.com]>
2026-04-13 20:56:16 -0300

    Adicionar arquivo TopoAdm.php com estrutura de cabeçalho e link para CSS

---
+++ Paginas/TopoAdm.php
@@ -0,0 +1,18 @@
+<?php
+// Identifica se o arquivo que executou este código está na raiz ou na pasta Paginas
+$diretorioAtual = basename(dirname($_SERVER['PHP_SELF']));
+
+// Se estiver em 'Paginas', o prefixo para voltar à raiz é '../'
+// Caso contrário (na raiz), o prefixo é vazio ''
+$prefixo = ($diretorioAtual == 'Paginas') ? '../' : '';
+?>
+
+<link rel="stylesheet" href="<?php echo $prefixo; ?>FontEnd/Estilo.css">
+
+<header class="index-topo">
+    <div class="index-container topo-conteudo">
+        <div class="logo-marca">
+            <div class="logo-box">🔧</div>
+            <span>JP Auto Center</span>
+        </div>
+</header>
