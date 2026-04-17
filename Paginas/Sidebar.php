e41096377d0492572948438b02fc64a245f53b11
Natan <natan-lincoln@hotmail.com>
2026-04-16 21:28:37 -0300

    Refactor admin sidebar and header for improved structure and accessibility

--- Paginas/Sidebar.php
+++ Paginas/Sidebar.php
@@ -2,23 +2,47 @@
 $pagina_atual = basename($_SERVER['PHP_SELF']);
 ?>
 
-<aside class="admin-sidebar">
-    <div class="admin-sidebar-logo">
-        <img src="caminho/para/seu/logo-automaster.png" alt="AutoMaster">
-    </div>
+<aside class="adm-sidebar">
 
-    <nav class="admin-sidebar-nav">
+    <nav class="adm-sidebar-nav">
         <ul>
-            <li class="<?= ($pagina_atual == 'AgendarAdm.php') ? 'active' : '' ?>">
+
+            <li class="<?= ($pagina_atual == 'AreaAdmin.php') ? 'adm-active' : '' ?>">
+                <a href="AreaAdmin.php">
+                    <i class="fas fa-th-large"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li class="<?= ($pagina_atual == 'Clientes.php') ? 'adm-active' : '' ?>">
+                <a href="Clientes.php">
+                    <i class="fas fa-users"></i>
+                    <span>Clientes</span>
+                </a>
+            </li>
+
+            <li class="<?= ($pagina_atual == 'AgendarAdm.php') ? 'adm-active' : '' ?>">
                 <a href="AgendarAdm.php">
-                    <i class="fas fa-calendar-alt"></i> Agendamentos
+                    <i class="fas fa-calendar-alt"></i>
+                    <span>Agendamentos</span>
                 </a>
             </li>
+
+            <li class="<?= ($pagina_atual == 'OrdemServico.php') ? 'adm-active' : '' ?>">
+                <a href="OrdemServico.php">
+                    <i class="fas fa-file-invoice"></i>
+                    <span>Ordens de Serviço</span>
+                </a>
+            </li>
+
             <li>
-                <a href="Logout.php" class="link-voltar">
-                    <i class="fas fa-sign-out-alt"></i> Voltar para o site
+                <a href="Logout.php">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <span>Voltar para o site</span>
                 </a>
             </li>
+
         </ul>
     </nav>
+
 </aside>
--- Paginas/Topo.php
+++ Paginas/Topo.php
@@ -1,29 +1,15 @@
 <?php
-// Identifica se o arquivo que executou este código está na raiz ou na pasta Paginas
 $diretorioAtual = basename(dirname($_SERVER['PHP_SELF']));
-
-// Se estiver em 'Paginas', o prefixo para voltar à raiz é '../'
-// Caso contrário (na raiz), o prefixo é vazio ''
 $prefixo = ($diretorioAtual == 'Paginas') ? '../' : '';
 ?>
 
 <link rel="stylesheet" href="<?php echo $prefixo; ?>FontEnd/Estilo.css">
 
-<header class="index-topo">
-    <div class="index-container topo-conteudo">
+<header class="adm-topas">
+    <div class="topo-conteudo">
         <div class="logo-marca">
             <div class="logo-box">🔧</div>
             <span>JP Auto Center</span>
         </div>
-
-        <nav class="menu-topo">
-            <a href="<?php echo $prefixo; ?>Index.php" class="ativo">Início</a>
-            
-            <a href="<?php echo $prefixo; ?>Index.php#servicos">Serviços</a>
-            
-            <a href="<?php echo ($diretorioAtual == 'Paginas') ? '' : 'Paginas/'; ?>Agendar.php">Agendar</a>
-            
-            <a href="<?php echo ($diretorioAtual == 'Paginas') ? '' : 'Paginas/'; ?>AreaAdmin.php" class="btn-admin">Área Admin</a>
-        </nav>
     </div>
 </header>
