3c4dc8b10b7c4a391d674197766d1182e2f6dda3
Bbasilisco <beatrizfonsecabasilisco@gmail.comb>
2026-04-16 21:05:38 -0300

    side bar

--- Paginas/Sidebar.php
+++ Paginas/Sidebar.php
@@ -9,24 +9,9 @@ $pagina_atual = basename($_SERVER['PHP_SELF']);
 
     <nav class="admin-sidebar-nav">
         <ul>
-            <li class="<?= ($pagina_atual == 'AreaAdmin.php') ? 'active' : '' ?>">
-                <a href="AreaAdmin.php">
-                    <i class="fas fa-th-large"></i> Dashboard
-                </a>
-            </li>
-            <li class="<?= ($pagina_atual == 'Clientes.php') ? 'active' : '' ?>">
-                <a href="Clientes.php">
-                    <i class="fas fa-users"></i> Clientes
-                </a>
-            </li>
             <li class="<?= ($pagina_atual == 'AgendarAdm.php') ? 'active' : '' ?>">
                 <a href="AgendarAdm.php">
                     <i class="fas fa-calendar-alt"></i> Agendamentos
-                </a>
-            </li>
-            <li class="<?= ($pagina_atual == 'OrdemServico.php') ? 'active' : '' ?>">
-                <a href="OrdemServico.php">
-                    <i class="fas fa-file-invoice"></i> Ordens de Serviço
                 </a>
             </li>
             <li>
